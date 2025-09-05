8f1461287d5e9499fc754344ab67d83e33ac298c
efaller <efaller@brazpine.com>
2025-09-05 00:04:02 -0300

    feat: port from deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,8 @@
 import { config } from 'dotenv'
 import { z } from 'zod'
 
-if(process.env.NODE_ENV === 'test'){
-  config({ path: '.env.test'})
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
 } else {
   config()
 }
@@ -17,9 +17,8 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error("Invalid environment variables!", _env.error.type)
-
-  throw new Error("Invalid environment variables.")
+  console.error('Invalid environment variables!', _env.error.format())
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-
 import { app } from './app.js'
 import { env } from './env/index.js'
 
 app.listen({
+  host: '0.0.0.0', // Render precisa disso
   port: env.PORT
 }).then(() => {
-  console.log('HTTP Server Running!')
+  console.log(`🚀 HTTP Server Running on port ${env.PORT}`)
 })
